817501bd45272dffaff6e7fa9b6d7b1b1543cc7d
UbuntuDev <juan.agudelo@outlook.com>
2017-04-16 21:49:36 -0500

    set admin lte desiign

--- resources/views/brand/index.blade.php
+++ resources/views/brand/index.blade.php
@@ -1,35 +1,48 @@
 @extends('layout')
 
 @section('content')
-
-<div>
-	<h2>Brands</h2>
-
+<!--<div class="row placeholders">-->
+	<h1 class="page-header">Crear marca</h1>
 	<!-- will be used to show any messages -->
 	@if (Session::has('message'))
     	<div class="alert alert-info">{{ Session::get('message') }}</div>
 	@endif
 
 	<p>
-		<a class="btn btn-primary" href="/brand/create">Add a brand</a>
+		<a class="btn btn-primary" href="/brand/create">Crear marca</a>
 	</p>
-
-	<ul>
-		@foreach ($brands as $brand)
-		<li>
-			<span>{{ $brand->brand_name }}</span>
-			<a href="/brand/edit/{{ $brand->brand_id }}" class="btn btn-info">Edit</a>
-			<form method="POST" action="/brand/destroy/{{ $brand->brand_id }}">
-				<input type="hidden" name="_token" value={{ csrf_token() }}>
-				<input type="hidden" name="_method" value="DELETE">
-				<button type="submit" class="btn btn-danger">Eliminar</button>
-			</form>
-		</li>
-		@endforeach
-	</ul>
-	
+  <div class="table-responsive">
+            
+         
+  <table id="grid" class="table table-striped">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Nombre</th>
+                  <th colspan="2">Acciones</th>
+                </tr>
+              </thead>
+              <tbody>
+			          @foreach ($brands as $brand)
+                  <tr>
+                    <td>{{ $brand->brand_id }}</td>
+                    <td>{{ $brand->brand_name }}</td>
+                    <td><a href="/brand/edit/{{ $brand->brand_id }}" class="btn btn-info">Editar</a></td>
+                    <td>
+                        <form method="POST" action="/brand/destroy/{{ $brand->brand_id }}">
+                          <input type="hidden" name="_token" value={{ csrf_token() }}>
+                          <input type="hidden" name="_method" value="DELETE">
+                          <button type="submit" class="btn btn-danger">Eliminar</button>
+                        </form>
+                    </td>
+                    
+                  </tr>
+                @endforeach
+              </tbody>
+  </table>
+	 </div>
 	{{ $brands->links() }}
+<!--</div>-->
 
-</div>
 
 @endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,20 +1,169 @@
 <!DOCTYPE html>
+<!--
+This is a starter template page. Use this page to start your new project from
+scratch. This page gets rid of all links and provides the needed markup only.
+-->
 <html>
 <head>
-	<meta charset="utf-8">
-	<title></title>
-	<link rel="stylesheet" type="text/css" href="/css/app.css">
-	@yield('styles')
+    <meta charset="UTF-8">
+    <title>{{ $page_title or "Find Parking" }}</title>
+    <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+    <!-- Bootstrap 3.3.2 -->
+    <link href="{{ asset("/bower_components/admin-lte/bootstrap/css/bootstrap.min.css") }}" rel="stylesheet" type="text/css" />
+    <!-- Font Awesome Icons -->
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+    <!-- Ionicons -->
+    <link href="http://code.ionicframework.com/ionicons/2.0.0/css/ionicons.min.css" rel="stylesheet" type="text/css" />
+    <!-- Theme style -->
+    <link href="{{ asset("/bower_components/admin-lte/dist/css/AdminLTE.min.css")}}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset("/bower_components/admin-lte/plugins/datatables/dataTables.bootstrap.min.css") }}"></script>
+    
+    <!-- AdminLTE Skins. We have chosen the skin-blue for this starter
+          page. However, you can choose any other skin. Make sure you
+          apply the skin class to the body tag so the changes take effect.
+    -->
+    <link href="{{ asset("/bower_components/admin-lte/dist/css/skins/skin-blue.min.css")}}" rel="stylesheet" type="text/css" />
 
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
 </head>
-<body>
+<body class="skin-blue">
+<div class="wrapper">
 
-<div class="container">
-	<h1>Layout h1</h1>
-	@yield('content')
-</div>
+    <!-- Header -->
+    @include('header')
 
-@yield('scripts')
+    <!-- Sidebar -->
+    @include('sidebar')
 
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <h1>
+                {{ $page_title or "Page Title" }}
+                <small>{{ $page_description or null }}</small>
+            </h1>
+            <!-- You can dynamically generate breadcrumbs here -->
+            <ol class="breadcrumb">
+                <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
+                <li class="active">Here</li>
+            </ol>
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
+            <!-- Your Page Content Here -->
+            @yield('content')
+        </section><!-- /.content -->
+    </div><!-- /.content-wrapper -->
+
+    <!-- Footer -->
+    @include('footer')
+
+</div><!-- ./wrapper -->
+
+<!-- REQUIRED JS SCRIPTS 
+@yield('scripts')-->
+<!-- jQuery 2.1.3 -->
+<script src="{{ asset ("/bower_components/admin-lte/plugins/jQuery/jquery-2.2.3.min.js") }}"></script>
+<!-- Bootstrap 3.3.2 JS -->
+<script src="{{ asset ("/bower_components/admin-lte/bootstrap/js/bootstrap.min.js") }}" type="text/javascript"></script>
+<!-- AdminLTE App -->
+<script src="{{ asset ("/bower_components/admin-lte/dist/js/app.min.js") }}" type="text/javascript"></script>
+<script src="{{ asset ("/bower_components/admin-lte/plugins/datatables/jquery.dataTables.min.js") }}"></script>
+<script src="{{ asset ("/bower_components/admin-lte/plugins/datatables/dataTables.bootstrap.min.js") }}"></script>
+
+
+
+<script>
+$(document).ready(function() {
+    $('#grid').DataTable();
+} );
+</script>
+<!-- Optionally, you can add Slimscroll and FastClick plugins.
+      Both of these plugins are recommended to enhance the
+      user experience -->
 </body>
 </html>
+
+{{-- <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Administrador FindParking</title>
+
+    <link rel="stylesheet" type="text/css" href="/css/app.css">
+    <link rel="stylesheet" type="text/css" href="/css/admin.css">
+    
+	  @yield('styles')
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="/js/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Administrador FindParking</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#">Perfil</a></li>
+            <li><a href="#">Ayuda</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-sm-3 col-md-2 sidebar">
+          <ul class="nav nav-sidebar">
+            <li class="active"><a href="#">Marcas vehiculos <span class="sr-only">(current)</span></a></li>
+            <li><a href="/brands"></a></li>
+            <!--<li><a href="#">Analytics</a></li>
+            <li><a href="#">Export</a></li>-->
+          </ul>
+          
+         <!-- <ul class="nav nav-sidebar">
+            <li><a href="">Nav item again</a></li>
+            <li><a href="">One more nav</a></li>
+            <li><a href="">Another nav item</a></li>
+          </ul>-->
+        </div> 
+        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+          @yield('content')
+        </div>
+      </div>
+    </div>
+    @yield('scripts')
+  </body>
+</html>
+ --}}

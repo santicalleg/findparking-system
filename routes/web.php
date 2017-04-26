667dee0b51c88f26d36230dd45eaea4270ef5490
santicalleg <santiagocalleg@gmail.com>
2017-04-25 23:08:47 -0500

    Add logout

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,8 @@ Route::prefix('admin')->group(function() {
 	Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
 	Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login');
 
+	Route::post('/logout', 'Auth\AdminLoginController@logout')->name('admin.logout');
+
 	Route::get('/register', 'Auth\AdminRegisterController@showRegistrationForm')->name('admin.register');
 	Route::post('/register', 'Auth\AdminRegisterController@register')->name('admin.register');
 });

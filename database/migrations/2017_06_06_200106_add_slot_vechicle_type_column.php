648da7be85d6d35751dafae6a2aee5ea78587809
Santiago Calle <santiago.calle@doctus.com.co>
2017-06-06 15:03:32 -0500

    Add migration to add vechicle_type column to slots

---
+++ database/migrations/2017_06_06_200106_add_slot_vechicle_type_column.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddSlotVechicleTypeColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('slot', function(Blueprint $table) {
+            $table->integer('vehicle_type_id')->unsigned()->defautl(1);
+            $table->foreign('vehicle_type_id')->references('id')->on('vehicle_type');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::table('slot', function(Blueprint $table) {
+            $table->dropColumn('price');
+        });
+    }
+}

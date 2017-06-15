ff2abe8defbf16f356417fe8a7e9ee10e6a9f4cf
Santiago Calle <santiago.calle@doctus.com.co>
2017-06-15 13:54:08 -0500

    Update social icon links

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -6,13 +6,13 @@
     <!-- Default to the left -->
     <strong class="pull-left" >Copyright © {{ date('Y')}} <a href="#">Cindy - Jaime</a>.</strong>
     <div class="social-icons">
-        <a href="https://www.facebook.com/">
+        <a target="_blank" href="https://www.facebook.com/">
             <i class="fa fa-facebook" aria-hidden="true"></i>
         </a>
-        <a href="https://twitter.com/">
+        <a target="_blank" href="https://twitter.com/">
             <i class="fa fa-twitter" aria-hidden="true"></i>
         </a>
-        <a href="https://www.instagram.com">
+        <a target="_blank" href="https://www.instagram.com">
             <i class="fa fa-instagram" aria-hidden="true"></i>
         </a>
     </div>
